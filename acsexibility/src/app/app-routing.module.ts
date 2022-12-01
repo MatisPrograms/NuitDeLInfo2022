e07eec530610a7cceccc9d61a13044f01b4e12cb
Matis Herrmann <matis.herrmann@etu.unice.fr>
2022-12-01 23:15:42 +0100

    Added basic components

--- acsexibility/src/app/app-routing.module.ts
+++ acsexibility/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {GamesComponent} from './games/games.component';
+import {HomepageComponent} from './homepage/homepage.component';
+import {ErrorpageComponent} from "./errorpage/errorpage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomepageComponent},
+  {path: 'games', component: GamesComponent},
+  {path: '**', component: ErrorpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- acsexibility/src/app/app.module.ts
+++ acsexibility/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { GamesComponent } from './games/games.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    GamesComponent,
+    ErrorpageComponent
   ],
   imports: [
     BrowserModule,
